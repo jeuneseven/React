6c01283332be087b23c69fa460632ded235a3c90
jeuneseven <jeuneseven@gmail.com>
2025-06-24 02:32:51 +0800

    Update _layout.jsx

--- MyApp/app/(tabs)/_layout.jsx
+++ MyApp/app/(tabs)/_layout.jsx
@@ -14,7 +14,7 @@ export default function TabLayout() {
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         tabBarInactiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-        // headerTitleAlign: "center",
+        headerTitleAlign: "center",
       }}>
       <Tabs.Screen
         name="index"
